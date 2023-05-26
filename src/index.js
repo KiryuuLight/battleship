84c88865fdfdaf53da7a42b869f252256d0179c0
Luis Ortiz <70501251+KiryuuLight@users.noreply.github.com>
2023-05-26 15:55:50 -0500

    Add game initialization logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-const a = 2;
+// import CustomConsole from './console';
+
+// window.test = CustomConsole('Luis');
+
+import './css/style.css';
+import gameController from './controller/gameController';
+
+let newGame = gameController('Luis');
+
+const resetGame = () => {
+    newGame = gameController('Luis');
+    newGame.bindOnAppResetGame(resetGame);
+};
+
+newGame.bindOnAppResetGame(resetGame);
